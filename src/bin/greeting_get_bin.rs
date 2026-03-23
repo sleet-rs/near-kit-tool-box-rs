98ed356cd956244bafbd6c7870bd9d6e44c8224b
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-23 17:48:00 -0500

    Update greeting_get_bin.rs

--- src/bin/greeting_get_bin.rs
+++ src/bin/greeting_get_bin.rs
@@ -1,12 +1,11 @@
 // get_greeting
 //
-use near_kit::Error;
+use near_kit::*;
 use near_kit_tool_box::fun::greeting::greeting_get_fun::get_greeting_fun;
-use near_kit_tool_box::lib::client::NearClient;
 // =================================================
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let near = NearClient::testnet().build();
+    let near = Near::testnet().build();
     let contract_id = "hello.sleet.testnet";
     let greeting = get_greeting_fun(&near, contract_id).await?;
     println!("Current Greeting: {greeting}");
