5a3c7a611ab2acd3cc90d8b96801f41d9ff7a8e0
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-07-20 22:57:46 -0500

    Update mod.rs

--- src/fun/mod.rs
+++ src/fun/mod.rs
@@ -1,3 +1,4 @@
+// =================================================
 //! Helper functions for interacting with NEAR contracts.
 pub mod greeting {
     /// a get greeting function for a near greeting contract.
@@ -8,5 +9,3 @@ pub mod greeting {
     pub mod greeting_set_fun_json;
 }
 // =================================================
-// =================================================
-// copyright 2026 by sleet.near
